1975143110ce785e72debcbacfe51a8bc9747f8b
Jason Kuo <j20001970@gmail.com>
2022-04-25 13:52:48 +0800

    update GDMP usage

--- README.md
+++ README.md
@@ -12,5 +12,5 @@ Current supported platforms:
 The demo comes with precompiled libraries, simply export the project to supported platform and make sure associated plugins are enabled in export options. You can also build the libraries yourself.
 
 1. Follow [Getting Started](https://github.com/j20001970/GDMP#getting-started) to complete the setup.
-2. Add calculator dependencies in `mediapipe/GDMP/variables.bzl`
+2. Add calculator dependencies in `mediapipe/GDMP/calculators.bzl`
 3. Run `build.py` with the same usage as `GDMP/build.py`, the script will overlay mediapipe workspace with files in `mediapipe` during build process.
--- project/pose_tracking/PoseTracking.gd
+++ project/pose_tracking/PoseTracking.gd
@@ -2,7 +2,7 @@ extends Node
 
 var graph : GDMP.Graph
 var camera_helper : GDMP.CameraHelper
-var gpu_helper
+var gpu_helper : GDMP.GPUHelper
 
 func _ready() -> void:
 	$TextureRect.texture = ImageTexture.new()
@@ -10,11 +10,24 @@ func _ready() -> void:
 	graph.initialize("res://mediapipe/graphs/pose_tracking/pose_tracking_gpu.pbtxt", true)
 	graph.add_packet_callback("output_video", self, "_on_new_frame")
 	graph.add_packet_callback("pose_landmarks", self, "_on_new_pose")
-	gpu_helper = graph.get_gpu_helper()
+	gpu_helper = GDMP.GPUHelper.new()
+	gpu_helper.initialize(graph)
 	camera_helper = GDMP.CameraHelper.new()
+	camera_helper.connect("permission_granted", self, "_on_permission_granted")
+	camera_helper.connect("permission_denied", self, "_on_permission_denied")
 	camera_helper.set_graph(graph, "input_video")
 	graph.start()
-	camera_helper.start(GDMP.CAMERA_FACING_FRONT)
+	if camera_helper.permission_granted():
+		camera_helper.start(GDMP.CAMERA_FACING_FRONT, Vector2(640, 480))
+	else:
+		camera_helper.request_permission()
+
+func _on_permission_granted() -> void:
+	print("permission granted")
+	camera_helper.start(GDMP.CAMERA_FACING_FRONT, Vector2(640, 480))
+
+func _on_permission_denied() -> void:
+	print("permission denied")
 
 func _on_new_frame(stream_name : String, packet) -> void:
 	var image : Image = gpu_helper.get_gpu_frame(packet)
