e47bc403251a85c206d81aa915ae1eadf3c96042
Jason Kuo <j20001970@gmail.com>
2025-03-03 00:31:36 +0800

    add tests

---
+++ project/tests/test_proto.gd
@@ -0,0 +1,22 @@
+extends GutTest
+
+func test_uninitialized():
+	var proto := MediaPipeProto.new()
+	assert_false(proto.is_initialized())
+	assert_eq(proto.get_type_name(), "")
+	assert_true(proto.get_fields().is_empty())
+	assert_false(proto.is_repeated_field(""))
+	assert_eq(proto.get_repeated_field_size(""), 0)
+	assert_null(proto.get_field(""))
+	assert_null(proto.get_repeated_field("", 0))
+	assert_false(proto.set_field("", 0))
+	assert_null(proto.duplicate())
+
+func test_simple():
+	var proto := MediaPipeProto.new()
+	assert_true(proto.initialize("mediapipe.Color"))
+	assert_true(proto.is_initialized())
+	assert_eq(proto.get_type_name(), "mediapipe.Color")
+	assert_true(proto.get_fields().has("r"))
+	assert_true(proto.get_fields().has("g"))
+	assert_true(proto.get_fields().has("b"))
