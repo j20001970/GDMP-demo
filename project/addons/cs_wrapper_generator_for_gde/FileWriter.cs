a0ede6413e6c293f2cbe08e32742dc9dd280a0b1
Jason Kuo <j20001970@gmail.com>
2025-03-02 23:22:43 +0800

    add C# wrapper generator for GDExtension addon

---
+++ project/addons/cs_wrapper_generator_for_gde/FileWriter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace GDExtensionAPIGenerator;
+
+internal static class FileWriter
+{
+    internal static void WriteResult(IReadOnlyList<(string fileName, string fileContent)> codes)
+    {
+        DirAccess.MakeDirAbsolute(GeneratorMain.WRAPPERS_PATH);
+        
+        foreach (var (fileName, fileContent) in codes)
+        {
+            if(fileContent is null) continue;
+            using var fileAccess = FileAccess.Open(GeneratorMain.GetWrapperPath(fileName), FileAccess.ModeFlags.Write);
+            fileAccess.StoreString(fileContent);
+        }
+
+        EditorInterface
+            .Singleton
+            .GetResourceFilesystem()
+            .Scan();
+    }
+}
