51bce4a09201d319e8114ccac9c70d2d87575836
Jason Kuo <j20001970@gmail.com>
2025-03-08 09:54:54 +0800

    add CameraServerExtension addon

---
+++ project/addons/CameraServerExtension/plugin.gd
@@ -0,0 +1,10 @@
+@tool
+extends EditorPlugin
+
+var android_export_plugin: EditorExportPlugin = preload("android_export_plugin.gd").new()
+
+func _enter_tree() -> void:
+	add_export_plugin(android_export_plugin)
+
+func _exit_tree() -> void:
+	remove_export_plugin(android_export_plugin)
